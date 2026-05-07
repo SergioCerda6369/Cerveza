12c90873b2dd221209babbd4ada903f3e444c097
Sergio <ser.cerdal@duocuc.cl>
2026-05-07 01:36:33 -0400

    Agregando microservicio completo de StockFinal y ajuste en pedido

--- src/main/java/com/cerveza/cerveza/model/StockFinal.java
+++ src/main/java/com/cerveza/cerveza/model/StockFinal.java
@@ -1,13 +1,11 @@
 package com.cerveza.cerveza.model;
 
-import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
